c292fb86f08a7e439337c0b8580724202c84cebf
aougost <agustns.koo@gmail.com>
2021-11-10 13:18:20 +0700

    first commit

---
+++ src/components/Customer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" class="customer-name">
+        name: {{ name }}
+      </v-col>
+      <v-col cols="12" sm="6" md="4" class="customer-age">
+        age: {{ age }}
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Customer",
+
+  props: {
+    name: String,
+    age: Number,
+  }
+}
+</script>
+
+<style>
+.customer-name {
+  color: red;
+}
+.customer-age {
+  color: green;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col v-for="greeting in greetings" :key="greeting" cols="12" sm="6" md="4">
+        {{ greeting }} test
+      </v-col>
+    </v-row>
+    <v-btn @click="onClick">
+      Click me
+    </v-btn>
+    <div v-for="c in customers" :key="c.id">
+      <Customer
+        v-show="c.id != 1"
+        :name="c.name"
+        :age="c.age"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Customer from './Customer'
+
+export default {
+  name: 'HelloWorld',
+
+  components: {
+    Customer,
+  },
+
+  mounted() {
+
+  },
+
+  data: () => ({
+    greetings: ["Hello", "Goodbye", "Hola"],
+    customers: [
+      { id: 1, name: "Joko", age: 20 },
+      { id: 2, name: "Budi", age: 20 },
+      { id: 3, name: "Tono", age: 20 },
+      { id: 4, name: "Tina", age: 20 },
+      { id: 5, name: "Tini", age: 20 },
+      { id: 6, name: "Susi", age: 20 },
+      { id: 7, name: "Adhi", age: 20 },
+    ],
+  }),
+
+  methods: {
+    onClick() {
+      const newCustomers = this.customers.map(c => ({ ...c, age: c.age * 2}))
+      this.customers = newCustomers
+
+    /* Contoh update data ke API
+      await updateData(newCustomers) // Kirim data ke backend
+
+      this.customers = await getData()
+      */
+    },
+    async fetchData() {
+      // bikin request ke api
+      // this.customers = await requestKeApi()
+
+      // const newCustomers = this.customers.map(c => ({ ...c, age: c.age * 2}))
+    }
+  }
+}
+</script>
+
+<style>
+.test {
+  height: 200px;
+  width: 200px;
+  background-color: red;
+}
+</style>
